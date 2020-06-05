04dc011d929eefc165b232fb8b2aa3e0591ebe93
PythonNurs <nursultankaragulov0220@gmail.com>
2020-06-05 18:52:13 +0600

    add pagination and filter

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,11 +1,14 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
 from .serializers import ProductListSerializer, ProductSerializer
 from .models import Product
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', ]
 
 
 class ProductDetailView(RetrieveAPIView):
