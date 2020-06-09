35b9fda639388a6e22df25715a523b613e9aae81
Nurdoolot Sonunbekov <rayfi.vv@gmail.com>
2020-06-09 12:27:42 +0600

    Create transliterate function

--- shop/utils.py
+++ shop/utils.py
@@ -1,18 +1,86 @@
 import random
 import string
 from django.utils.text import slugify
-
+import re
 
 def random_string_generator(size=15, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-# def translitirate_chars()
+def transliterate(string):
+
+    capital_letters = {u'А': u'A',
+                       u'Б': u'B',
+                       u'В': u'V',
+                       u'Г': u'G',
+                       u'Д': u'D',
+                       u'Е': u'E',
+                       u'Ё': u'E',
+                       u'З': u'Z',
+                       u'И': u'I',
+                       u'Й': u'Y',
+                       u'К': u'K',
+                       u'Л': u'L',
+                       u'М': u'M',
+                       u'Н': u'N',
+                       u'О': u'O',
+                       u'П': u'P',
+                       u'Р': u'R',
+                       u'С': u'S',
+                       u'Т': u'T',
+                       u'У': u'U',
+                       u'Ф': u'F',
+                       u'Х': u'H',
+                       u'Ъ': u'',
+                       u'Ы': u'Y',
+                       u'Ь': u'',
+                       u'Э': u'E',}
+
+    lower_case_letters = {u'а': u'a',
+                       u'б': u'b',
+                       u'в': u'v',
+                       u'г': u'g',
+                       u'д': u'd',
+                       u'е': u'e',
+                       u'ё': u'e',
+                       u'ж': u'zh',
+                       u'з': u'z',
+                       u'и': u'i',
+                       u'й': u'y',
+                       u'к': u'k',
+                       u'л': u'l',
+                       u'м': u'm',
+                       u'н': u'n',
+                       u'о': u'o',
+                       u'п': u'p',
+                       u'р': u'r',
+                       u'с': u's',
+                       u'т': u't',
+                       u'у': u'u',
+                       u'ф': u'f',
+                       u'х': u'h',
+                       u'ц': u'ts',
+                       u'ч': u'ch',
+                       u'ш': u'sh',
+                       u'щ': u'sch',
+                       u'ъ': u'',
+                       u'ы': u'y',
+                       u'ь': u'',
+                       u'э': u'e',
+                       u'ю': u'yu',
+                       u'я': u'ya',}
+    
+    for dictionary in (capital_letters, lower_case_letters):
+
+        for cyrillic_string, latin_string in dictionary.items():
+            string = string.replace(cyrillic_string, latin_string)
+
+    return string
 
 def slug_generator(obj, new_slug=None):
     if new_slug is not None:
-        slug = new_slug
+        slug = transliterate(new_slug)
     else:
-        slug = slugify(obj.title, allow_unicode=True)
+        slug = transliterate(slugify(obj.title, allow_unicode=True))
 
     
     Klass = obj.__class__
@@ -20,6 +88,7 @@ def slug_generator(obj, new_slug=None):
     if queryset_exsists:
         slug = slug
         randomstring = random_string_generator()
+        
         new_slug = f'{slug}-{randomstring}'
         return slug_generator(obj, new_slug=new_slug)
     else:
